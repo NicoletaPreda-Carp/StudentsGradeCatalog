83723276b7d088b0ce5d1d8644aeea15bc931e2d
Nicoleta_Preda_Carp <nellyc007@yahoo.com>
2020-01-27 18:27:39 +0200

    define class field and constructor
    Signed-off-by: Nicoleta_Preda_Carp <nellyc007@yahoo.com>

---
+++ src/com/company/Student.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Student {
+    private String name;
+    private String surname;
+    private int age;
+    private int gradeMath;
+    private int gradeEnglish;
+    private int gradeSport;
+
+    public Student (String name, String surname) {
+        
+    }
+
+}

0a5a878e6f27ce9b01e49c18f228d8ac0732bbdf
Nicoleta_Preda_Carp <nellyc007@yahoo.com>
2020-01-30 01:30:02 +0200

    Initialize class Student.
    Signed-off-by: Nicoleta_Preda_Carp <nellyc007@yahoo.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Student jhon = new Student("John", "Dale", 10);
+	Student kille = new Student("Kille", "Jason", 9);
+	Student marryN = new Student("Marry", "Nicholson", 10);
+	Student marryF = new Student("Marry", "Flinstone", 10);
     }
 }

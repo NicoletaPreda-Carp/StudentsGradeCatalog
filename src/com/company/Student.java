3a019059be8db6080477355c6f09490854c72525
Nicoleta_Preda_Carp <nellyc007@yahoo.com>
2020-01-30 00:57:58 +0200

    Make getter and setter for class fields
    Define the class constructor.
    
    Signed-off-by: Nicoleta_Preda_Carp <nellyc007@yahoo.com>

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -8,8 +8,59 @@ public class Student {
     private int gradeEnglish;
     private int gradeSport;
 
-    public Student (String name, String surname) {
-        
+    public Student (String name, String surname, int age) {
+        name = this.name;
+        surname = this.surname;
+        age = this.age;
     }
 
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGradeMath() {
+        return gradeMath;
+    }
+
+    public void setGradeMath(int gradeMath) {
+        this.gradeMath = gradeMath;
+    }
+
+    public int getGradeEnglish() {
+        return gradeEnglish;
+    }
+
+    public void setGradeEnglish(int gradeEnglish) {
+        this.gradeEnglish = gradeEnglish;
+    }
+
+    public int getGradeSport() {
+        return gradeSport;
+    }
+
+    public void setGradeSport(int gradeSport) {
+        this.gradeSport = gradeSport;
+    }
 }
